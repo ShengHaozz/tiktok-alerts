505158424b2a1021dbc7d223fed1dd19cdf8c912
shenghao <shenghaoong@gmail.com>
2025-07-09 00:51:21 +0800

    added send message functionality to telegram connector

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import logging
 import os
 from telegram_connector import TelegramConnector
+import threading
+import time
+import asyncio
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.DEBUG,
-    filename="logging.log"
+    filename="logging.log",
 )
 
 API_TOKEN = os.getenv("RINGOBOT_API_TOKEN")
 
-telegramConn = TelegramConnector(API_TOKEN)
-telegramConn.run()
+telegram_conn = TelegramConnector(API_TOKEN)
+telegram_conn_thread = threading.Thread(target=telegram_conn.run)
+telegram_conn_thread.start()
+
+time.sleep(5)
+telegram_conn.broadcast("5 seconds")
+time.sleep(5)
+telegram_conn.broadcast("10 seconds")
+
+
+telegram_conn.stop()
--- telegram_connector.py
+++ telegram_connector.py
@@ -12,6 +12,25 @@ class TelegramConnector:
         self.logger = logging.getLogger(__name__)
         self.tick_duration = tick_duration
 
+        self.application = ApplicationBuilder().token(self._token).build()
+
+        handlers = []
+
+        handlers.append(CommandHandler("test", self._test))
+        handlers.append(CommandHandler("sub", self._subscribe))
+        handlers.append(CommandHandler("unsub", self._unsubscribe))
+        handlers.append(CommandHandler("check_sub", self._check_subscription))
+
+        self.application.add_handlers(handlers)
+
+    async def _send_message(
+        self,
+        chat_id: int,
+        message: str,
+    ):
+        bot = self.application.bot
+        await bot.send_message(chat_id=chat_id, text=message)
+
     async def _test(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         user = update.effective_user
         chat = update.effective_chat
@@ -19,7 +38,7 @@ class TelegramConnector:
 
         output_message = f"{user.username} sent {message.text} in {'private chat' if chat.type == Chat.PRIVATE else chat.title}"
         self.logger.info(output_message)
-        await context.bot.send_message(chat_id=user.id, text=output_message)
+        await self._send_message(user.id, output_message)
 
     async def _subscribe(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         user = update.effective_user
@@ -31,7 +50,7 @@ class TelegramConnector:
             self.subscribers[user.id] = f"@{user.username}"
             output_message = f"@{user.username} subscribed"
         self.logger.info(output_message)
-        await context.bot.send_message(chat_id=chat.id, text=output_message)
+        await self._send_message(chat.id, output_message)
 
     async def _unsubscribe(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         user = update.effective_user
@@ -43,33 +62,19 @@ class TelegramConnector:
         else:
             output_message = f"@{user.username} not subscribed"
         self.logger.info(output_message)
-        await context.bot.send_message(chat_id=chat.id, text=output_message)
+        await self._send_message(chat.id, output_message)
 
-    async def _check_subscription(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def _check_subscription(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ):
         chat = update.effective_chat
-        await context.bot.send_message(chat_id=chat.id, text=list(self.subscribers.values()))
+        await self._send_message(chat.id, "\n".join(self.subscribers.values()))
 
-
-    async def _tick(self, context: ContextTypes.DEFAULT_TYPE):
-        subscribers_id = self.subscribers.keys()
-        await asyncio.gather(
-            *[
-                context.bot.send_message(chat_id=user_id, text="tick")
-                for user_id in subscribers_id
-            ]
-        )
+    async def broadcast(self, message):
+        await asyncio.gather(*[self._send_message(id, message) for id in self.subscribers.keys()])
 
     def run(self):
-        application = ApplicationBuilder().token(self._token).build()
+        self.application.run_polling()
 
-        handlers = []
-
-        handlers.append(CommandHandler("test", self._test))
-        handlers.append(CommandHandler("sub", self._subscribe))
-        handlers.append(CommandHandler("unsub", self._unsubscribe))
-        handlers.append(CommandHandler("check_sub", self._check_subscription))
-
-        application.add_handlers(handlers)
-        application.job_queue.run_repeating(self._tick, self.tick_duration, name="ticker")
-
-        application.run_polling()
+    def stop(self):
+        self.application.stop_running()
